5b7ecfebaa6a2609895ffadc069cc85ba82a53c3
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-02-28 12:06:34 +0100

    add dom rendering

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 
-//has to be declared as empty 
+const rowEl = document.querySelector(".row")
+//console.log(rowEl);
 
-const postsArray = []
+
+//fetch section
+//has to be declared as empty 
+//const postsArray = []
 fetch("https://lanciweb.github.io/demo/api/pictures/")
     .then((response) => response.json())
     .then((result) => {
@@ -9,20 +13,43 @@ fetch("https://lanciweb.github.io/demo/api/pictures/")
         console.log(`begin foreach to transfer posts from result array to postsArray`);
 
         result.forEach((element, i) => {
-            postsArray.push(element)
+            //postsArray.push(element)
             console.log(`this is element ${i + 1}:`);
             console.log(element);
-
+            renderPosts(element)
         });
 
     })
     .catch((error) => console.error(error));
 
-console.log(`This is the array containing the received posts: `);
-console.log(postsArray);
+//console.log(`This is the array containing the received posts: `);
+//console.log(postsArray);
 
 
 
 
 //functions
+
+function renderPosts(object) {
+    const markup = `<div class="col">
+                    <div class="card">
+                        <img class="center-pin" src="/assets/img/pin.svg" alt="pin">
+
+                        <div class="img-container">
+                            <img src="${object.url}" alt="post-photo">
+                        </div>
+
+                        <div class="text-container">
+                            <span>${object.title}
+                            </span>
+                            <span>${object.date}
+                            </span>
+                        </div>
+                    </div>
+                </div>`
+
+
+
+    rowEl.innerHTML += markup
+}
 
